660a78bbf88a2a06fdfaf9d974e14373e31963a9
SantyVilchez <santiagovilchez20@gmail.com>
2025-10-03 19:26:51 -0300

    agrego codigo huffman terminado

--- Practica 4/Ejercicio 3/arbol_huffman.py
+++ Practica 4/Ejercicio 3/arbol_huffman.py
@@ -1,4 +1,5 @@
 from nodo_arbol import Nodo
+from lista_enlazada import ListaEncadenada
 import csv
 class ArbolHuffman:
     __raiz : Nodo
@@ -7,27 +8,33 @@ class ArbolHuffman:
         self.__raiz = None
         self.__dic = {}
         self.crear_dict()
+        self.estructurador()
     def crear_dict(self):
         with open('texto.csv', newline='', encoding='utf-8') as archivo:
             filas = csv.reader(archivo)
             for fila in filas:
-                for columna in fila:  
-                    for caracter in columna:
-                        c = caracter.lower()
+                for columna in fila:
+                    columna = columna.split(' ')
+                    for palabra in columna:
+                        c = palabra.lower()
                         if c in self.__dic:
                             self.__dic[c] += 1
                         else:
                             self.__dic[c] = 1
     def estructurador(self):
-        lista = [1,2,3]
-        lista.pop()
-        print(lista)
-        lista_nodos = [Nodo(int(f), v) for v, f in self.__dic.items()]
-        lista_nodos.sort(key=lambda nodo: nodo.getFrecuencia())
-        # while len(lista_nodos) != 1:
-        #     nuevo_valor = lista_nodos[0].getValor()+lista_nodos[1].getValor()
-        #     nueva_frecuencia = lista_nodos[0].getFrecuencia() + lista_nodos[1].getFrecuencia()
-        #     nuevo_nodo = Nodo.
+        lista_nodos = ListaEncadenada()
+        for valor,frecuencia in self.__dic.items():
+            lista29_nodos.insertar(Nodo(frecuencia,valor))
+        while lista_nodos.cantidad() != 1:
+            primero = lista_nodos.suprimir()
+            segundo = lista_nodos.suprimir()
+            nuevo_valor = primero.getValor() + segundo.getValor()
+            nueva_frecuencia = primero.getFrecuencia() + segundo.getFrecuencia()
+            nuevo_nodo = Nodo(nueva_frecuencia,nuevo_valor)
+            nuevo_nodo.setIzq(primero)
+            nuevo_nodo.setDer(segundo)
+            lista_nodos.insertar(nuevo_nodo)
+        self.__raiz = lista_nodos.cabeza().getElem()
     def insertar(self, valor :int):
         if not self.__raiz:
             self.__raiz = Nodo(valor)
@@ -47,157 +54,60 @@ class ArbolHuffman:
                 self._insertar(nodo.getDer(),valor)
             else:
                 nodo.setDer(Nodo(valor))
-  
-    def inOrden(self):
-        self._inOrden(self.__raiz)
-
-    def _inOrden(self,nodo:Nodo):
-        if nodo:
-            self._inOrden(nodo.getIzq())
-            print(nodo.getValor())#10/4/
-            self._inOrden(nodo.getDer())
-
-    def buscar(self,valor : int)->Nodo:
-        return self._buscar(self.__raiz,valor)
-
-    def _buscar(self, nodo : Nodo, valor : int):
-        if not nodo:
-            nodo_encontrado = None
-        elif nodo.getValor() == valor:
-            nodo_encontrado = nodo
-        elif valor < nodo.getValor():
-            nodo_encontrado = self._buscar(nodo.getIzq(),valor)
-        else:
-            nodo_encontrado = self._buscar(nodo.getDer(),valor)
-        return nodo_encontrado
-
-    def grado(self,nodo : Nodo):
-        grado = 0
-        if nodo.getIzq():
-            grado+=1
-        if nodo.getDer():
-            grado+=1
-        return grado
-    def suprimir(self,valor:int):
-        self._suprimir(self.__raiz,valor)
-    def _suprimir(self,nodo : Nodo, valor : int):
-        if not nodo:
-            return None
-        elif valor < nodo.getValor():
-            nodo.setIzq(self._suprimir(nodo.getIzq(),valor))
-        elif valor > nodo.getValor():
-            nodo.setDer(self._suprimir(nodo.getDer(),valor))
-        else :
-            if self.grado(nodo) == 0:
-                return None
-            if self.grado(nodo) == 1:
-                return nodo.getDer() if nodo.getDer() else nodo.getIzq()
-            if self.grado(nodo) == 2:
-                maximo = nodo.getIzq()
-                while maximo.getDer() != None:
-                    maximo = maximo.getDer()
-                nodo.setValor(maximo.getValor())
-                nodo.setIzq(self._suprimir(nodo.getIzq(), maximo.getValor()))
-        return nodo
-    
-    def hoja(self,valor:int):
-        nodo = self.buscar(valor)
-        es_hoja = False
-        if not nodo:
-            es_hoja = False
-        elif self.grado(nodo)==0:
-            es_hoja = True
-        return es_hoja
-    
-    def hijo(self, hijo : int, padre : int):
-        nodo_padre = self.buscar(padre)
-        es_hijo = False
-        if nodo_padre == None:
-            es_hijo = False
-        elif nodo_padre.getIzq() != None and nodo_padre.getIzq().getValor() == hijo:
-            es_hijo = True
-        elif nodo_padre.getDer() != None and nodo_padre.getDer().getValor()==hijo:
-            es_hijo = True
-        return es_hijo
-    def padre(self, padre:int, hijo:int):
-        nodo_padre = self.buscar(padre)
-        es_padre = False
-        if nodo_padre == None:
-            es_padre = False
-        elif nodo_padre.getIzq() != None and nodo_padre.getIzq().getValor() == hijo:
-            es_padre = True
-        elif nodo_padre.getDer() != None and nodo_padre.getDer().getValor()==hijo:
-            es_padre = True
-        return es_padre
-    def nivel(self,valor : int):
-        return self._nivel(self.__raiz,valor,0)
-    def _nivel(self, nodo : Nodo, valor : int,nivel : int):
-        if nodo == None:
-            nivel = 0
-        elif valor < nodo.getValor():
-            nivel += 1
-            nivel = self._nivel(nodo.getIzq(),valor,nivel)
-        elif valor > nodo.getValor():
-            nivel += 1
-            nivel = self._nivel(nodo.getDer(),valor,nivel)
-        else:
-            nivel+=1
-        return nivel
-    def altura(self):
-        return self._altura(self.__raiz,0)
-    
-    def _altura(self, nodo : Nodo, altura : int):
-        if nodo:
-            if altura < self.nivel(nodo.getValor()):
-                altura +=1
-            altura = self._altura(nodo.getIzq(),altura)
-            altura = self._altura(nodo.getDer(),altura)
-        return altura
-
-    def PreOrden(self):
-        self._PreOrden(self.__raiz)
-
-    def _PreOrden(self,nodo:Nodo):
-        if nodo:
-            print(nodo.getValor())
-            self._inOrden(nodo.getIzq())
-            self._inOrden(nodo.getDer())
- 
-    def PostOrden(self):
-        self._PostOrden(self.__raiz)
-
-    def _PostOrden(self,nodo:Nodo):
-        if nodo:
-            self._inOrden(nodo.getIzq())
-            self._inOrden(nodo.getDer())
-            print(nodo.getValor())
-    def raiz(self):
-        return self.__raiz
-    def antecesor(self,x : int,z : int):
-        es_antecesor = False
-        if self._buscar(self.buscar(x),z) != None:
-            es_antecesor = True
-        return es_antecesor
-        
-    def camino(self,inicio : int,fin : int):
-        es_antecesor = self.antecesor(inicio, fin)
-        camino = []
-        if es_antecesor == True:
-            nodo_actual = self.buscar(inicio)
-            while nodo_actual != None and nodo_actual.getValor() != fin:
-                if fin < nodo_actual.getValor():
-                    camino.append(0)
+    def codificador(self,valor : int):
+        raiz = self.__raiz
+        camino = ""
+        if raiz != None:
+            nodo_actual = raiz
+            while nodo_actual != None and nodo_actual.getValor() != valor:
+                if nodo_actual.getIzq() and valor in nodo_actual.getIzq().getValor():
+                    camino+="0"
                     nodo_actual = nodo_actual.getIzq()
                 else:
-                    camino.append(1)
+                    camino+="1"
                     nodo_actual = nodo_actual.getDer()
         return camino
-
-
-
-Arbol = ArbolHuffman()
-
-Arbol.estructurador()
-
-
-
+    def cuadro_decodificador(self):
+        with open('texto.csv', newline='', encoding='utf-8') as archivo:
+            filas = csv.reader(archivo)
+            for fila in filas:
+                for columna in fila:
+                    columna = columna.split(' ')
+                    for palabra in columna:
+                        print(f"Palabra: {palabra}\ncodigo: {self.codificador(palabra)}")
+    def codificador_archivo(self):
+        with open('texto.csv','r') as archivo_entrada:
+            with open('texto_codificado.csv', 'w') as archivo_salida:
+                lector = csv.reader(archivo_entrada)
+                escritor = csv.writer(archivo_salida)
+                for fila in lector:
+                    for renglon in fila:
+                        renglon = renglon.split(" ")
+                        nueva_fila = [self.codificador(palabra) for palabra in renglon]
+                        escritor.writerow(nueva_fila)
+    def decodificador(self, valor : str):
+        nodo = self.__raiz
+        for i in valor:
+            if i == "0":
+                nodo = nodo.getIzq()
+            else:
+                nodo = nodo.getDer()
+        return nodo.getValor()
+                        
+        
+    def decodificador_archivo(self):
+        with open('texto_codificado.csv','r') as archivo_entrada:
+            with open('texto_decodificado.csv', 'w') as archivo_salida:
+                lector = csv.reader(archivo_entrada)
+                escritor = csv.writer(archivo_salida)
+                for fila in lector:
+                    for renglon in fila:
+                        nueva_fila = [self.decodificador(palabra) for palabra in renglon]
+                        escritor.writerow(nueva_fila)
+                        
+    def Raiz(self):
+        return self.__raiz
+arbol = ArbolHuffman()
+arbol.cuadro_decodificador()
+print(arbol.codificador("minuto"))
+print(arbol.decodificador("0001101"))
--- Practica 4/Ejercicio 3/lista_enlazada.py
+++ Practica 4/Ejercicio 3/lista_enlazada.py
@@ -1,10 +1,11 @@
-class Nodo:
-    __elem : int
+from nodo_arbol import Nodo
+class Nodo_lista:
+    __elem : object
     __sig : None
-    def __init__(self,elem : int)->None:
+    def __init__(self,elem : Nodo)->None:
         self.__elem = elem
         self.__sig = None
-    def getElem(self)->int:
+    def getElem(self)->object:
         return self.__elem
     def setElem(self,elem)->None:
         self.__elem = elem
@@ -14,32 +15,36 @@ class Nodo:
         self.__sig = sig
 
 class ListaEncadenada:
-    __cabeza : Nodo
+    __cabeza : Nodo_lista
     __cant : int
     def __init__(self):
         self.__cabeza = None
         self.__cant = 0
     def vacia(self):
         return self.__cant == 0
-    def insertar(self,elem:int):
-        nuevo = Nodo(elem)
-        if self.__cabeza is None or elem < self.__cabeza.getElem():
+    def insertar(self,elem:Nodo):
+        nuevo = Nodo_lista(elem)
+        if self.__cabeza is None or elem.getFrecuencia() < self.__cabeza.getElem().getFrecuencia():
             nuevo.setSig(self.__cabeza)
             self.__cabeza = nuevo
         else:
             anterior = self.__cabeza
-            while anterior.getSig() != None and elem > anterior.getSig().getElem():
+            while anterior.getSig() != None and elem.getFrecuencia() > anterior.getSig().getElem().getFrecuencia():
                 anterior = anterior.getSig()
             nuevo.setSig(anterior.getSig())
             anterior.setSig(nuevo)
         self.__cant += 1
-    def suprimir(self):
+    def suprimir(self)->Nodo:
         self.__cant-=1 
-        borrado = self.__cabeza 
+        borrado = self.__cabeza.getElem() 
         self.__cabeza = self.__cabeza.getSig() 
         return borrado 
     def recorrer(self):
         aux = self.__cabeza
         while aux != None:
-            print(aux.getElem())
+            print(aux.getElem().getFrecuencia())
             aux = aux.getSig()
+    def cantidad(self):
+        return self.__cant
+    def cabeza(self):
+        return self.__cabeza
